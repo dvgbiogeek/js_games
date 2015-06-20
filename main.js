d1e94ab2b0cb79a218c15ead954de550c1f93630
glickdv <dvgbiogeek@gmail.com>
2015-06-19 23:06:34 -0700

    working tic tac toe
    added a game

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-function clicked(id) {
-	console.log("clicked " + id);
-} 
-
-
-// Game manager 
-	// 1. creating a new game
-	// 2. handling the game loop
-	// 3. check if there is a winner
-		// a. player gets 3 in a row
-		// b. all the spaces are full
-
 angular.module('gameApp', ['ngRoute'])
 
 	.controller('playerController', ['$location', 'PlayerService',
 		function($location, PlayerService) {
 		this.genPlayer = false;
-		// this.playerTurn = false;
 		this.duplicates = true;
 		
 		// Get player information
@@ -34,15 +21,48 @@ angular.module('gameApp', ['ngRoute'])
 		this.playerTemplate = 'player.html';
 	
 	}])
-	.controller('gameController', ['PlayerService', function(PlayerService) {
+	.controller('gameController', ['PlayerService', 'BoardService',
+		function(PlayerService, BoardService) {
 		var ticPlayers = PlayerService.getPlayers();
-		console.log(ticPlayers);
-		this.first = PlayerService.firstPlayer(ticPlayers, 2);
-		console.log(this.first);
+		var first = PlayerService.firstPlayer(ticPlayers, 2);
 		
+		this.showFirst = true;
+		this.firstPlayer = first;
+		this.playTime = false;
+		var turnOffset = first.id;
+		
+		this.activePlayer = this.firstPlayer; 
+		this.board = BoardService.newBoard(3);
+		this.gameBoard = BoardService.getBoard();
+		this.playerWins = false;
+		this.stalemate = false;
+
+		this.clicked = function(objId) {
+			this.showFirst = false;
+			this.playTime = true;
+			this.displayTurn = PlayerService.displayPlayer();
+			var elementId = Number(objId.toElement.id);
+			this.player = PlayerService.activePlayer();
+			BoardService.addMark(elementId, this.player);
+			this.playerWins = BoardService.playerWin(this.player, 3);
+			if (!this.playerWins) {
+				this.stalemate = BoardService.isStalemate();
+			}
+		}; 
+
+		this.newGame = function() {
+			first = PlayerService.firstPlayer(ticPlayers, 2);
+			this.firstPlayer = first;
+			this.board = BoardService.newBoard(3);
+			this.gameBoard = BoardService.getBoard();
+			this.playerWins = false;
+			this.playTime = false;
+			this.showFirst = true;
+			this.stalemate = false;
+		};
+
 		// Template for the board
 		this.ticTemplate = 'ticboard.html';
-		// console.log(PlayerService.getPlayers());
 	}])
 
 	.config(function($routeProvider) {
@@ -55,15 +75,83 @@ angular.module('gameApp', ['ngRoute'])
 			controller: 'gameController'
 		});
 	})
+	.factory('BoardService', [function() {
+		var board;
+
+		return {
+			getBoard: function() {
+				return board;
+			},
+			newBoard: function(size) {
+				board = [];
+				turn = 0;
+				var boardSize = size * size;
+				for (var i=0; i < boardSize; i++) {
+					board.push("");
+				}
+				return board;
+			},
+			addMark: function(elementId, player) {
+				board[elementId] = player.symbol;
+				return board;
+			},
+			playerWin: function(player, bSize) {
+				var win;
+				for (var i = 0; i < bSize; i++) {
+					if (board[0 + i * bSize] == player.symbol && board[1 + i * bSize] == player.symbol && board[2 + i * bSize] == player.symbol) {
+						win = true;
+					}
+					else if (board[i] == player.symbol && board[i + 1 * bSize] == player.symbol && board[i + 2 * bSize] == player.symbol) {
+						win = true;
+					}
+				}
+				var diag1 = [];
+				for (i = 0; i < bSize; i++) {
+					var diagPosition = bSize * i + i;
+					diag1.push(board[diagPosition]); 
+				}
+				var firstDiag = new Set(diag1);
+				if (firstDiag.size == 1 && firstDiag.has(player.symbol)) {
+					win = true;
+				}
+				var diag2 = [];
+				for (i = 0; i < bSize; i++) {
+					var diagPosition2 = (bSize - 1) + 2 * i;
+					diag2.push(board[diagPosition2]);
+				}
+				var secDiag = new Set(diag2);
+				if (secDiag.size == 1 && secDiag.has(player.symbol)) {
+					win = true;
+				}
+				if (win) {
+					return true;
+				} else {
+					return false;
+				}					
+			},
+			isStalemate: function() {
+				var stalemate;
+				for (i = 0; i < board.length; i++) {
+					if (board[i] === '') {
+						stalemate = false;
+						return stalemate;
+					} 
+				}
+				stalemate = true;
+				return stalemate;
+			}
+		};
+	}])
 
 	.factory('PlayerService', [function() {
 		var players;
+		var turn;
+		var turnOffset;
 
 		return {
 			// Saves the list of players as a variable on the factory
 			playerList: function(playerArray) {
 				players = playerArray;
-				console.log(players);
 				return players;
 			},
 			// retreives the list of players
@@ -78,20 +166,25 @@ angular.module('gameApp', ['ngRoute'])
 				}
 				return players;
 			},
-			// returns 0 or 1 (min = 0, max = 2) for determining which player goes first
-			randomPlayer: function(min, max) {
-				// Math.floor returns an integer without rounding, Math.random generates a number between 0 and 1 
-				return Math.floor(Math.random() * (max - min)) + min;
-			},
 			firstPlayer: function(playerArray, max) {
-				console.log('calling firstPlayer');
 				// min is equal to 0 in this case
-				var turn = Math.floor(Math.random() * max);
-				console.log(turn);
-				var first = playerArray[turn];
-				console.log(first);
+				turn = 0;
+				turnOffset = Math.floor(Math.random() * max);
+				var first = playerArray[turnOffset];
 				return first;
 			},
+			// returns the player whose just took their turn 
+			activePlayer: function() {
+				var playerTurn = players[(turn + turnOffset) % players.length];
+				turn += 1;
+				return playerTurn;
+			},
+			// Displays the current player  
+			displayPlayer: function() {
+				var display = players[(turn + turnOffset + 1) % players.length];
+				return display;
+			},
+			// function for validating that the players entered unique values for their names and board symbols
 			playersValid: function(playerArray) {
 				// get a list of all the keys in the array
 				var key = Object.keys(playerArray[0]);
